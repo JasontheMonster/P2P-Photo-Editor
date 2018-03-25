d3b1f71bf0b485427e535e22b8c73404bd90366d
AZ's Macbook <zhaoyz@bc.edu>
2018-03-25 19:55:33 -0400

    finalized APIs

--- node.go
+++ node.go
@@ -5,6 +5,10 @@ import (
     "fmt"
     "net"
     "sync"
+    "flag"
+    "bufio"
+    "strings"
+    "os"
 )
 
 var (
@@ -20,18 +24,12 @@ type Node struct {
     conn_list   map[int]net.Conn
 }
 
-//Invitation message 
-type Invite struct{
-    accept     bool
-    mem_list   []Node  
-}
-
 type Message struct {
-    helloworld    string
-    QUIT          bool   
+    Kind        string  // "INVITE", PUBLIC"
+    Msg         string
+    Mem_list    []Node
+    //QUIT          bool   
 }
-
-
 
 func (node *Node) delNode(id int) {
     node.conn_list[id].Close()
@@ -40,10 +38,10 @@ func (node *Node) delNode(id int) {
 
 
 //add Peer to the network
-func (node *Node) joinGroup(inv Invite){
-    for _, peer := range inv.mem_list{
+func (node *Node) joinGroup(mem_list []Node){
+    for _, peer := range mem_list{
         //connect to the peer
-        conn := connectPeer(peer.addr)
+        conn := node.connectPeer(peer.addr)
         //append peer node to the mem_list
         node.mem_list = append(node.mem_list, peer)
         //set active mem map with id = true
@@ -51,6 +49,7 @@ func (node *Node) joinGroup(inv Invite){
         //store connection in a map
         node.conn_list[peer.ID] = conn
     }
+    fmt.Println(node.mem_list)
 }
 
 func (node *Node) isAlive(id int) bool {
@@ -58,9 +57,9 @@ func (node *Node) isAlive(id int) bool {
     return status || prs
 }
 
-//receive server
-func (node *Node) receive(){
-    tcpAddr, err := net.ResolveTCPAddr("tcp4", ":6666")
+//creates server
+func (node *Node) server(nonstop chan bool){
+    tcpAddr, err := net.ResolveTCPAddr("tcp4", node.addr)
     if err != nil{
         fmt.Println("Err getting TCP.")
     }
@@ -76,27 +75,31 @@ func (node *Node) receive(){
             continue
         }
 
-        go handleMsg(conn)
-    }  
+        go node.handleMsg(conn)
+    } 
+    nonstop <- true
 }
 
 //function to handle message
-func handleMsg(conn net.Conn){
+func (node *Node) handleMsg(conn net.Conn){
     dec := json.NewDecoder(conn)
     msg:= new(Message)
     defer conn.Close()
     for {
-        err := dec.Decode(&msg)
-        if msg.QUIT {
+        if err := dec.Decode(msg); err != nil {
             return
-        }else{
-        fmt.Println(msg.helloworld, err)
+        }
+        switch msg.Kind {
+            case "INVITE":
+                node.joinGroup(msg.Mem_list)
+            case "PUBLIC":
+                fmt.Println(msg.Msg)
         }
     }
 }
 
 // create a connection to peer (string destination address)
-func connectPeer(addr string) net.Conn{
+func (node *Node) connectPeer(addr string) net.Conn{
     tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
     if err != nil{
         fmt.Println("Err getting addr.")
@@ -106,14 +109,13 @@ func connectPeer(addr string) net.Conn{
     if err2 != nil{
         fmt.Println("Err connection addr.")
     }
-  
     return conn 
 }
 
 // send invitation to new peer (string destination address)
 func (node *Node) invite(dest string) {
-    inv := Invite{accept: false, mem_list: node.mem_list}
-    conn := connectPeer(dest)
+    inv := createMessage("INVITE", "", node.mem_list)
+    conn := node.connectPeer(dest)
     enc := json.NewEncoder(conn)
     enc.Encode(inv)
 }
@@ -127,15 +129,48 @@ func (node *Node) sendToAll(msg Message) {
 }
 
 func main() {
+    nonstop := make(chan bool)
     var (
         node Node
         memlist []Node
     )
-    node.ID = 1
-    node.addr = "127.0.0.1:8888"
-    node.mem_list = memlist
+    flag.IntVar(&node.ID, "id", 0, "specify the node id")
+    flag.StringVar(&node.addr, "addr", "127.0.0.1:8080", "specify the node address")
+    flag.Parse()
     node.active_mem = make(map[int]bool)
     node.conn_list = make(map[int]net.Conn)
+    memlist = append(memlist, node)
+    node.mem_list = memlist
+    go node.server(nonstop)
+    go node.userInput(nonstop)
+    <- nonstop
+}
+
+func createMessage(Kind string, Msg string, Mem_list []Node) Message {
+    var msg Message
+    msg.Kind = Kind
+    msg.Msg = Msg
+    msg.Mem_list = Mem_list
+    return msg
+}
+
+func (node *Node) userInput(nonstop chan bool) {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Println("Enter command:")
+    for {
+        text,_ := reader.ReadString('\n')
+        text = strings.Replace(text, "\n", "", -1)
+        input := strings.SplitN(text, " ", 2)
+        switch input[0] {
+            case "invite":
+                go node.invite(input[1])
+            case "send":
+                msg := createMessage("PUBLIC", input[1], make([]Node,0))
+                go node.sendToAll(msg)
+        }
+    }
+
+    nonstop <- true
 }
 
 
